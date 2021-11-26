9b56503e6cd64169ac62c29190c2b8c7d3576ad0
Tim Vahlbrock <tim@vahlbrock.de>
2021-11-26 16:21:04 +0100

    Moved FairScheduler class to fairScheduler file

--- src/test/suite/unit.test.ts
+++ src/test/suite/unit.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-import { FairScheduler } from '../../dojoClock';
+import { FairScheduler } from '../../fairScheduler';
 
 export const participantColors = [
     "Blue",
